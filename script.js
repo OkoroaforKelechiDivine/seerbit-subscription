93ffc14087819855a5667a6d4fceef1040b138ad
theRealNonso <okoroaforkelechi123@gmail.com>
2023-07-13 22:50:00 +0000

    added html file

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('virtualAccountForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('virtualAccountForm');
+    const formData = new FormData(form);
+
+    const json = {};
+    formData.forEach(function(value, key) {
+        json[key] = value;
+    });
+
+    fetch('http://localhost:3000/api/v2/virtual-accounts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json)
+    })
+        .then(function(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Error occurred. Please try again.');
+            }
+        })
+        .then(function(data) {
+            displayResponseMessage(data.message);
+        })
+        .catch(function(error) {
+            displayResponseMessage(error.message);
+        });
+});
+
+function displayResponseMessage(message) {
+    const container = document.getElementsByClassName('container')[0];
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    container.appendChild(messageElement);
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); // Parse JSON request body
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.post('/api/v2/virtual-accounts', async (req, res) => {
+    try {
+        const response = await axios.post(
+            'https://seerbitapi.com/api/v2/virtual-accounts/',
+            req.body, {
+                headers: req.headers
+            }
+        );
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        if (error.response && error.response.status) {
+            res.status(error.response.status).json({
+                error: error.response.data || 'Internal Server Error'
+            });
+        } else {
+            res.status(500).json({
+                error: 'Internal Server Error'
+            });
+        }
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
